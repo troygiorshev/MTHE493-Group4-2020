d86402e76bc0c2dbdd6fa970f211f8323137b177
Troy Giorshev <troygiorshev@gmail.com>
2021-02-03 18:10:13 -0500

    Add 30x13 grid layout

--- sim.py
+++ sim.py
@@ -26,14 +26,13 @@ import seaborn as sns
 
 #random.seed(1234)   # Set random seed for reproducability
 
-NUM_NODES = 1150
+NUM_NODES = 1500
 PROPORTION_S_THOUGHTS = 0.16
 S_THOUGHTS_THRESHOLD = 0.7
 S_THRESHOLD = 0.9
 
 # Values of each node.  [R,B]
 nodes = np.array([[0,0]]*NUM_NODES)
-
 
 def check_setup(G):
     '''Check that the variables make sense with each other.  Graph G'''
@@ -53,6 +52,92 @@ def connected_graph():
     ])
     return nx.from_numpy_array(adj)
 
+def fill_houses():
+    ROWS = 13
+    COLS = 30
+    # Houses by number of bedrooms
+    layout = np.array([
+        [1,2,2,2,2,3,3,3,3,3,3,4,4,4,4,5,5,5,5,3,3,3,3,3,3,2,2,2,2,1],
+        [1,2,2,2,2,3,3,3,3,3,3,4,4,4,4,5,5,5,5,3,3,3,3,3,3,2,2,2,2,1],
+        [1,2,2,2,2,3,3,3,3,3,3,4,4,4,4,5,5,5,5,3,3,3,3,3,3,2,2,2,2,1],
+        [1,2,2,2,2,3,3,3,3,3,3,4,4,4,4,5,5,5,5,3,3,3,3,3,3,2,2,2,2,1],
+        [1,2,2,2,2,3,3,3,3,3,3,4,4,4,4,5,5,5,5,4,3,3,3,3,3,3,2,2,2,1],
+        [1,2,2,2,2,3,3,3,3,3,3,4,4,4,4,5,5,5,5,4,3,3,3,3,3,3,2,2,2,1],
+        [1,2,2,2,2,3,3,3,3,3,3,4,4,4,5,5,5,5,5,4,3,3,3,3,3,3,2,2,2,1],
+        [1,2,2,2,2,3,3,3,3,3,3,4,4,4,5,5,5,5,5,4,3,3,3,3,3,3,2,2,2,2],
+        [1,2,2,2,2,3,3,3,3,3,3,4,4,4,5,5,5,5,5,4,3,3,3,3,3,3,2,2,2,1],
+        [1,2,2,2,2,3,3,3,3,3,3,4,4,4,5,5,5,5,5,3,3,3,3,3,3,2,2,2,2,1],
+        [1,2,2,2,2,3,3,3,3,3,3,4,4,4,5,5,5,5,5,3,3,3,3,3,3,2,2,2,2,1],
+        [1,2,2,2,2,3,3,3,3,3,3,4,4,4,5,5,5,5,5,3,3,3,3,3,3,2,2,2,2,1],
+        [1,2,2,2,2,3,3,3,3,3,3,4,4,4,5,5,5,5,5,3,3,3,3,3,3,2,2,2,2,1]
+        ])
+    # Houses by number of people in them
+    houses = np.full_like(layout, 0)
+
+    people_left = NUM_NODES
+    # First, fill every bedroom with a person
+    for row, row_of_lots  in enumerate(layout):
+        for col, lot in enumerate(row_of_lots):
+            houses[row,col] = lot
+            people_left -= lot
+    
+    # Randomly empty 5(ish) interior houses
+    for _ in range(5):
+        row = random.randint(1, ROWS-2)
+        col = random.randint(1, COLS-2)
+        people_left += houses[row,col]
+        houses[row,col] = 0
+
+    # Keep adding people randomly to the 4 and 5 person houses
+    # While ensuring that no house goes over 15
+    while people_left > 0:
+        # This is a terribly slow way to do this
+        # But it still happens pretty much instantly
+        row = random.randint(0, ROWS-1)
+        col = random.randint(0, COLS-1)
+        if layout[row,col] < 4:
+            continue
+        if houses[row,col] >= 15:
+            continue
+        houses[row,col] += 1
+        people_left -= 1
+    
+    print(houses)
+
+    # TODO surely there's a clearer way to initialize a 2x2 array...
+    subgraphs = [[0 for _ in range(COLS)] for _ in range(ROWS)]
+
+    for row, row_of_houses in enumerate(houses):
+        for col, num_residents in enumerate(row_of_houses):
+            if num_residents != 0:
+                subgraphs[row][col] = nx.complete_graph(num_residents)
+            else:
+                subgraphs[row][col] = nx.null_graph()
+
+    subgraphs_flat = []
+    name_list = []
+    for i, row_of_subgraphs in enumerate(subgraphs):
+        for j, subgraph in enumerate(row_of_subgraphs):
+            if len(subgraph) > 0:   # Dont add the empty houses
+                subgraphs_flat.append(subgraph)
+                name_list.append(f'h{i},{j}-')
+
+    G = nx.union_all(subgraphs_flat, name_list)
+
+    for i in range(ROWS):
+        for j in range(COLS):
+            if G.has_node(f'h{i},{j}-0'):
+                if G.has_node(f'h{i+1},{j}-0'):
+                    G.add_edge(f'h{i},{j}-0', f'h{i+1},{j}-0')
+                if G.has_node(f'h{i},{j+1}-0'):
+                    G.add_edge(f'h{i},{j}-0', f'h{i},{j+1}-0')
+
+    print(nx.is_connected(G))
+
+    nx.draw_networkx(G)     # This takes a few seconds...
+    plt.show()
+
+
 # add up _____
 #compute connectivity i.e. average degree (density of graph)
 
@@ -68,7 +153,7 @@ def init_nodes():
     num_unhealthy = int(adjust_for_real_stats())
     for node in nodes[0:num_unhealthy - 1]:
         node[0] = int(10 * S_THOUGHTS_THRESHOLD)
-        node[1] = int(10 - 10 * S_THOUGHTS_THRESHOLD)
+        node[1] = 10 - node[0]
     for node in nodes[num_unhealthy:]:
         tmp = random.randint(0, 5)  # 0 to 5 inclusive
         node[0] = tmp
@@ -184,6 +269,9 @@ def main():
     plt.title("After")
     plt.show()
 
+def main2():
+    fill_houses()
 
 if __name__ == "__main__":
-    main()
+    main2()
+    #main()
